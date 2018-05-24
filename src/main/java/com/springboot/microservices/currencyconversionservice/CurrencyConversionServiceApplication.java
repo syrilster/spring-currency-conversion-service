59621a6fccfb8599491afa4bdc22c5f4f7002c9f
Syril <syrilster@gmail.com>
2018-05-24 18:29:58 +0530

    Register CurrencyConversionService with Eureka Naming server on startup.

--- src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.springboot.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.springboot.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {

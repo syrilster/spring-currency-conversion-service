13dbe94042b53f42553de7b06ff15feb72265f92
Syril <syrilster@gmail.com>
2018-05-24 17:45:18 +0530

    After setting up client side LB using Ribbon

--- src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,13 @@
 package com.springboot.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConverter retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);

c315b6a24197462d9eb88d9a4aeae73fd4c32b78
Syril <syrilster@gmail.com>
2018-05-25 22:26:51 +0530

    After adding the ability to intercept a request and execute some logic. This is using Zuul.

--- src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //@GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //To execute the request via Zuul. i.e  "http://localhost:8765/{application-name}/{uri}"
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConverter retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }

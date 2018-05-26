5b29f0d0aa2c21c3058d2aef400045b142fa9392
Syril <syrilster@gmail.com>
2018-05-26 09:08:59 +0530

    After adding Slueth distributed tracing.

--- src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.springboot.microservices.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
 
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
@@ -36,6 +40,7 @@ public class CurrencyConversionController {
     public CurrencyConverter retrieveExchangeValueFeign(@PathVariable String from, @PathVariable String to,
                                                         @PathVariable BigDecimal quantity) {
         CurrencyConverter response = proxy.retrieveExchangeValue(from, to);
+        logger.info("{} " + response);
         return new CurrencyConverter(response.getId(), from, to, response.getConversionMultiple(),
                 quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
--- src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,9 +1,11 @@
 package com.springboot.microservices.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("com.springboot.microservices.currencyconversionservice")
@@ -12,5 +14,10 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }

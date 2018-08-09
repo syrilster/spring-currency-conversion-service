d404f3d999aaeb6d10b2e00fc0643de50aa737dc
Syril <syrilster@gmail.com>
2018-08-09 13:52:14 +1000

    Updated the comments.

--- src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/springboot/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
 @FeignClient(name = "netflix-zuul-api-gateway-server")
+/* If only using Ribbon then have the Feign Client without the URL
+@FeignClient(name = "currency-exchange-service")*/
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     //@GetMapping("/currency-exchange/from/{from}/to/{to}")
